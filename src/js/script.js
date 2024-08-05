b8804339a3774e1ff5231ab6b7d0f4ae9f74fe76
Kishan Sharma <kshar832@gmail.com>
2024-08-05 19:24:19 +0100

    italic

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('nameplate.md')
+    fetch('/src/md/nameplate.md')
         .then(response => response.text())
         .then(text => {
             const contentDiv = document.getElementById('nameplate');
@@ -16,5 +16,18 @@ function toggleClass(className) {
 
 // Add event listeners for buttons
 document.getElementById("bold").addEventListener("click", () => toggleClass("font-bold"));
-document.getElementById("italic").addEventListener("click", () => toggleClass("italic"));
 document.getElementById("underline").addEventListener("click", () => toggleClass("underline"));
+
+document.getElementById("italic").addEventListener("click", () => {
+    // see if checkbox is clicked
+    const italic = document.getElementById("italic").checked;
+    const italic_file = italic ? 'nameplate-italic.md' : 'nameplate.md';
+    fetch("/src/md/" + italic_file)
+        .then(response => response.text())
+        .then(text => {
+            const contentDiv = document.getElementById('nameplate');
+            contentDiv.innerText = text;
+        })
+        .catch(error => console.error('Error fetching the markdown file:', error));
+});
+
